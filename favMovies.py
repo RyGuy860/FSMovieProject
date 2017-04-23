266961a00362dfe6dba14847dbe39302ce1f6154
RyGuy860 <rnichols23@gmail.com>
2017-04-23 01:53:38 -0400

    updated files to me python line length restrictions.

--- favMovies.py
+++ favMovies.py
@@ -1,47 +1,61 @@
+# handles our frontend code
 import fresh_tomatoes
+# file that contains Trailers class
 import movies
+
+
+# list of movies including their Title, storyline and trailer
 
 troy = movies.Trailers(
                     "Troy",
-                    "An adaptation of Homer's great epic, the film follows the assault on Troy by the united Greek forces and chronicles the fates of the men involved.",
-                    "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk5MzU1MDMwMF5BMl5BanBnXkFtZTcwNjczODMzMw@@._V1_SY1000_CR0,0,677,1000_AL_.jpg",
+                    "An adaptation of Homer's great epic, the film follows\
+                     the assault on Troy by the united Greek forces and\
+                     chronicles the fates of the men involved.",
+                    "http://www.impawards.com/2004/posters/troy_ver2.jpg",
                     "https://www.youtube.com/watch?v=IKQhUzxlml8")
-print(troy.storyline)
+
 creed = movies.Trailers(
                     "Creed",
-                    "The former World Heavyweight Champion Rocky Balboa serves as a trainer and mentor to Adonis Johnson, the son of his late friend and former rival Apollo Creed.",
+                    "The former World Heavyweight Champion Rocky\
+                    Balboa serves as a trainer and mentor to Adonis\
+                    Johnson, the son of his late friend and\
+                    former rival Apollo Creed.",
                     "http://www.impawards.com/2015/posters/creed.jpg",
                     "https://youtu.be/Uv554B7YHk4")
 
-#print(avatar.storyline)
-
 alfie = movies.Trailers(
                     "Alfie",
-                    "A cockney womanizer learns the hard way about the dangers of his actions.",
-                    "https://images-na.ssl-images-amazon.com/images/M/MV5BNTBjOWQwZDEtZWRlYi00YzY0LTg3NDQtYzRiYjk2YjMyMzEyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+                    "A cockney womanizer learns the hard way\
+                    about the dangers of his actions.",
+                    "http://www.impawards.com/2004/posters/alfie_ver2.jpg",
                     "https://youtu.be/T-whOtC8aak")
 
 hitch = movies.Trailers(
                     "Hitch",
-                    "While helping his latest client woo the fine lady of his dreams, a professional date doctor finds that his game doesn't quite work on the gossip columnist with \
+                    "While helping his latest client woo the fine lady of his \
+                    dreams, a professional date doctor finds that\
+                    his game doesn't quite work on the gossip columnist with \
                     whom he's smitten.",
-                    "https://images-na.ssl-images-amazon.com/images/M/MV5BNzYyNzM2NzM2NF5BMl5BanBnXkFtZTcwNjg5NTQzMw@@._V1_SY1000_CR0,0,659,1000_AL_.jpg",
+                    "http://www.impawards.com/2005/posters/hitch_ver2.jpg",
                     "https://youtu.be/dMaq_pfxs-0")
 
 love_jones = movies.Trailers(
                     "Love Jones",
-                    "Darius Lovehall is a young black poet in Chicago who starts dating Nina Moseley, a beautiful and talented photographer. While trying to figure out if they've got a \
-                    love thing or are just kicking it",
-                    "https://images-na.ssl-images-amazon.com/images/M/MV5BMTY2NjYxMDczNV5BMl5BanBnXkFtZTcwODAwODAyMQ@@._V1_.jpg",
+                    "Darius Lovehall is a young black poet in\
+                    Chicago who starts dating Nina Moseley, a beautiful and\
+                    talented photographer. While trying to figure out if\
+                    they've got a love thing or are just kicking it",
+                    "http://www.impawards.com/1997/posters/love_jones.jpg",
                     "https://youtu.be/DOYPqcb_jig")
 
-love_and_basketball = movies.Trailers(
-                    "Love and Basketball",
-                    "Monica (Sanaa Lathan) and Quincy (Omar Epps) are two childhood friends who both aspire to be professional basketball players.",
-                    "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU4NjY4NTI5MF5BMl5BanBnXkFtZTYwNjQ4OTc3._V1_.jpg",
-                    "https://youtu.be/Ur83i6_BjbE")
+X_Men = movies.Trailers(
+                    "X-Men",
+                    "They are children of the atom, homo superior, the\
+                    next link in the chain of evolution.",
+                    "http://www.impawards.com/2000/posters/xmen_ver3.jpg",
+                    "https://youtu.be/d-vKUbZYol4")
 
 
-
-myMovies = [troy, creed, alfie, hitch, love_jones, love_and_basketball]
+# array of movies to be shown on fresh_tomatoes movie page
+myMovies = [troy, creed, alfie, hitch, love_jones, X_Men]
 fresh_tomatoes.open_movies_page(myMovies)
